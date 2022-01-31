a13560d757c6a391545086ab81eb717e40573926
Barry Krawchuk <thebarryk@gmail.com>
2022-01-31 11:38:12 -0500

    pearson_correlation_by_duration.py Compares pearson coefficients for test duration and elapsed time. - duration is provided by akts database - elapsed is the time from start of test to end of test - It's an odd chart because it computes the pearson correlation   coffiecient for gradually increasing total test duration. It's   trying to show that the shorter test duration effects the test   results. - In this case there's a strong difference between duration and   elapsed. - Program was refactored to chart more than one pearson at a time.

--- pearson_correlation_by_duration.py
+++ pearson_correlation_by_duration.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[6]:
 
 
 get_ipython().run_line_magic('matplotlib', 'notebook')
 # Pearson_by_limit: Pearson correlations for range of test durations.
+# Compares a list of durations so duration and elapsed can be compared.
+# duration is time given to the time it takes to do a test by database.
+# elapsed is the time form the start to the end of the test.
 # Graphs show that % passing scores is higher when test duration is shorter.
 # The pearson correlation is calculated for ranges of test duration decreased
 # from full range to just 5 min.
@@ -16,13 +19,17 @@ import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
 import numpy as np
 
+# -----------------------------------------------------------------------
 class Pearson_by_limit():
+
     def duration(self, lo=5., hi=100., inc=5.):
-        self.limits = np.arange(lo, hi+0.01*(hi-lo)/inc, inc)
-    def __init__(self, df, field, hi=100., inc=5.):
+        return np.arange(lo, hi+0.01*(hi-lo)/inc, inc)
+
+    def __init__(self, df, fields, lo=5., hi=100., inc=5.):
         self.df = df
-        self.duration(lo=self.df[field].min(), hi=hi, inc=inc)
-    def pearson(self, field):
+        self.limits = self.duration(lo=lo, hi=hi, inc=inc)
+
+    def xpearson(self, field):
         self.correlation = {}
         for limit in self.limits:
             self.short = self.df[self.df.duration <= limit]
@@ -31,33 +38,61 @@ class Pearson_by_limit():
             except:
                 self.correlation[limit] = 0
         return self.correlation
-    def __repr__(self):
-        w = ""
-        for lim, v in self.correlation.items():
-            w += f"Highest duration: {lim:>6}   pearson_correlation: {v:>8f}\n"
-        return w
 
-def plot_pearson(df):
+    def pearson(self, fields):
+        self.correlations = {}
+        for field in fields:
+            
+            self.correlation = {}
+
+            for limit in self.limits:
+                self.short = self.df[self.df.duration <= limit]
+                try:
+                    self.correlation[limit] = pearsonr(self.short["TotalScore"], self.short[field])[0]
+                except:
+                    self.correlation[limit] = 0
+                    
+            self.correlations[field] = self.correlation
+            
+        return self.correlations
+
+# -----------------------------------------------------------------------
+def plot_pearson(df, fields):
     fig, ax = plt.subplots(figsize=(10, 5))
-    plt.plot(df.limits, df.correlation.values(), "o")
+    for field in fields:
+        plt.plot(df.limits, df.correlations[field].values(), "o", label=field)
     mplcursors.cursor(hover=True)
-    ax.set_title("Pearson Coefficient vs Upper Limit of Duration (min)")
-    ax.set_xlabel("Maximum Duration (min)")
-    ax.set_ylabel("Pearson Coefficient")
+    ax.set_title(f"Pearson Coefficient vs Upper Limit of Session Duration (min)")
+    ax.set_xlabel(f"Maximum Duration (min)")
+    ax.set_ylabel(f"Pearson Coefficient")
+    ax.legend()
     plt.grid(visible=True)
     plt.show()
 
-def main(field):
+# -----------------------------------------------------------------------
+def main():
 
     df = dti.read_dmv_log(case=1)
     # Add column to evaluate difference between start and end time
     df["elapsed"]    = abs(df['TestEndDateTime'] - df['TestStartDateTime']).dt.total_seconds()/60.
     df["difference"] = (df.elapsed - df.duration)
 
-    pearson = Pearson_by_limit(df, field, hi=20., inc=0.25)
-    correlation = pearson.pearson(field)
-    plot_pearson(pearson)
+    pearson = Pearson_by_limit(df, ["duration", "elapsed"], lo=5., hi=20., inc=0.25)
+    correlations = pearson.pearson(["duration", "elapsed"])
+    plot_pearson(pearson, ["duration", "elapsed"])
     return df, pearson
 
-df, pearson = main("duration")
+df, pearson = main()
+
+
+# In[ ]:
+
+
+b
+
+
+# In[ ]:
+
+
+
 
