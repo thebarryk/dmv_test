42504bf48a5db3ca5b29c2910b1c87d5d99e473b
Barry Krawchuk <thebarryk@gmail.com>
2022-02-01 12:42:14 -0500

    passing_rate.py Plot both duration and elapsed on the same graph. - Augmented passing_rate and plot_passing_rate to receive list of fields - Uses itertools.cycle to cycle through options sequentially - Problem: line is not drawn for 2nd line even though dots are - ToDo: try using line_collection for multiple lines.

--- passing_rate.py
+++ passing_rate.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
+# In[1]:
 
 
+get_ipython().run_line_magic('matplotlib', 'notebook')
 # Show variation of success vs time spent taking the driver's test.
 # Add compare duration and elapsed time
 # The success rate is averaged over bins of time taken to do the test.
@@ -16,8 +17,9 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import mplcursors
+from itertools import cycle
 
-def passing_rate(df, field, limits):
+def passing_rate(df, fields, limits):
     # passing_rate: DataFrame with columns duration and rate of passed tests 
     # input:
     #    df ...... dmv dataframe with columns duration and Result
@@ -27,19 +29,21 @@ def passing_rate(df, field, limits):
     #    duration ... average of the upper and lower bounds of each interval
     #    rate ....... fraction of tests passed over total tests taken during the duration period
     
-    def rate(lo, hi, df, field):
+    def rate(lo, hi, df, fields):
         # Calc passing rate inside this interval [lo, hi) of the field
         df["inside"] = (df[field] >= lo) & (df[field] < hi)
         n = df.inside.sum()
         if n == 0:
-            return 0   # Should be np.nan so they can be ignored later
-        np = ( (df.inside) & (df.passed) ).sum()
-        return np/n   
+            return np.nan   # Should be np.nan so they can be ignored later
+        npassed = ( (df.inside) & (df.passed) ).sum()
+        return npassed/n   
     
+#     pf = pd.DataFrame( [x[0] for x in limits], [x[1] for x in limits], columns=["lo", "hi"] )
     pf = pd.DataFrame( { "lo": [x[0] for x in limits], "hi" : [x[1] for x in limits] } )
     pf["duration"] = 0.5 * (pf.lo + pf.hi)
-    pf["rate"] = pf.apply(lambda x: rate(x.lo, x.hi, df, field), axis=1)
-    pf["field"] = field
+    for field in fields:
+        pf[field+"_rate"] = pf.apply(lambda x: rate(x.lo, x.hi, df, field), axis=1)
+#     pf["field"] = field
     
     return pf
 
@@ -47,13 +51,20 @@ def duration_intervals(lo=5., hi=100., inc=5.):
     r = np.arange(lo, hi+0.01*(hi-lo)/inc, inc)
     return [ (r[i], r[i+1]) for i in range(len(r)-1) ] 
 
-def plot_passing_rate(pf, field):
+def plot_passing_rate(pf, fields):
+    linestyle = cycle(["-", "--"])
+    color = cycle(["red", "green"])
     fig, ax = plt.subplots(figsize=(10, 5))
-    plt.plot(pf.duration, pf.rate, "o", label=field)
+    for field in fields:
+        plt.plot(pf.duration, 
+                 pf[field+"_rate"],  "o",
+                 color=next(color),
+                 linestyle=next(linestyle), 
+                 label=field+"_rate")
     
     mplcursors.cursor(hover=True)
-    ax.set_title(f"Passing Rate vs {field} (min)")
-    ax.set_xlabel(f"{field} (min)")
+    ax.set_title(f"Passing Rate vs Duration (min)")
+    ax.set_xlabel(f"Duration (min)")
     ax.set_ylabel(f"Passing Rate")
     ax.legend()
     plt.grid(visible=True)
@@ -65,10 +76,14 @@ def main():
     df["elapsed"] = abs(df['TestEndDateTime'] - df['TestStartDateTime']).dt.total_seconds()/60.
     df["passed"]  = (df.Result=="P")
 
-    limits = duration_intervals(lo=5, hi=40., inc=1)
-    pf = passing_rate(df, "duration", limits)
+    limits = duration_intervals(lo=5, hi=40., inc=.5)
     
-    plot_passing_rate(pf, "duration")
+#     test_times = ["duration"]
+    test_times = ["duration", "elapsed"]
+    
+    pf = passing_rate(df, test_times, limits)
+    
+    plot_passing_rate(pf, test_times)
     
     return df, pf
 
