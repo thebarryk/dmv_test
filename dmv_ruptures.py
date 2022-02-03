5d633e196e8ee12c851ee97ccdbb417f884c1850
Barry Krawchuk <thebarryk@gmail.com>
2022-02-03 14:27:03 -0500

    dmv_ruptures.py Use window, pelt and binary methods for both duratio and elapsed time. - Migrated all graphs to display away from ruptures package. - Each changepoint method is a function - Graphing separated from changepoit calculation in own function - Added legend Coding Features - Fetched axes limits used to offset text in user reference frame

--- dmv_ruptures.py
+++ dmv_ruptures.py
@@ -15,94 +15,87 @@ get_ipython().run_line_magic('matplotlib', 'notebook')
 #      https://techrando.com/2019/08/14/a-brief-introduction-to-change-point-detection-using-python/
 
 import ruptures as rpt
-import traceback
-from itertools import cycle
 from ruptures.utils import pairwise
 
-from passing_rate import passing_rate, duration_intervals
 import pandas as pd
-import dmv_test_input
 import matplotlib.pyplot as plt
 import mplcursors
 import datetime as dt
-import xdmv_test_input as dti
+import traceback
+from itertools import cycle
 
-
-def main():
-    max_test_duration = 60
+# private. part of dmv_test
+import xdmv_test_input as dti    # ToDo: migrate changes to dmv_test_input
+from passing_rate import passing_rate, duration_intervals
+#---------------------------------------------------------------------------
+def find_passing_rate(case=1, field="duration"):
     # Read and prepare the dmv_akts. Case=1 ... sample ... =2 ... akts database
-    df = dti.read_dmv_log(case=1)
+
+    # Set shortest and longest duration to consider in minutes
+    # To reduce effect of wild variation
+    min_test_duration = 5
+    max_test_duration = 35
+    
+    df = dti.read_dmv_log(case=case)
+
     df["elapsed"] = abs(df['TestEndDateTime'] - df['TestStartDateTime']).dt.total_seconds()/60.
     df["passed"]  = (df.Result=="P")
 
-    # Calculate the passing rates before and after a list of duration change points
-    limits = duration_intervals(lo=2.8, hi=max_test_duration, inc=.5)
-    pr = passing_rate(df, ["duration"], limits)
+    # Break up the test duration into bins
+    bins = duration_intervals(lo=min_test_duration, hi=max_test_duration, inc=.5)
+    # Calculate passing_rate during each bin
+    pr = passing_rate(df, [field], bins)
 
-    # Return the akts and passing rate data so it can be analysed interactively.
-    return df, pr, limits
+    # Return the akts, passing rate and bins so it can be analysed interactively after run
+    return df, pr, bins
 
-#RUPTURES PACKAGE
-#Changepoint detection with the Pelt search method
+# RUPTURES PACKAGE
+
+#---------------------------------------------------------------------------
 def pelt_changepoint(points):
+    # Changepoint detection with the Pelt search method
     model="rbf"
     try:
         algo = rpt.Pelt(model=model).fit(points)
     except Exception as e:
         print(f"Unable to predict for {model=}")
         print("")
+        return
 
-    result = algo.predict(pen=10)
-
-    rpt.display(points, result, figsize=(fw, fh))
-
-    mplcursors.cursor(hover=True)
-    plt.title('Change Point Detection: Pelt Search Method')
-    plt.xlabel("Duration (m)")
-    plt.ylabel("Fraction who passed")
-    plt.subplots_adjust(top=0.90)
-    plt.subplots_adjust(bottom=0.15)
-    plt.subplots_adjust(left=.1)
-    plt.show()  
+    bkps = algo.predict(pen=10)
+    return bkps
 
 # pelt_changepoint(points)
 
-#Changepoint detection with the Binary Segmentation search method
+#---------------------------------------------------------------------------
 def binary_changepoint(points):
+    # Changepoint detection with the Binary Segmentation search method
     model = "l2"  
-    algo = rpt.Binseg(model=model).fit(points)
-    my_bkps = algo.predict(n_bkps=10)
-    # show results
-    # rpt.show.display(points, my_bkps, figsize=(fw, fh))
-    f_bkps = [ pr.duration.iloc[x-1] for x in my_bkps]
-    rpt.show.display(points, f_bkps, figsize=(fw, fh))
-    plt.subplots_adjust(top=0.90)
-    plt.subplots_adjust(bottom=0.15)
-    plt.subplots_adjust(left=.1)
-    mplcursors.cursor(hover=True)
-    plt.title('Change Point Detection: Binary Segmentation Search Method')
-    plt.xlabel("Duration (m)")
-    plt.ylabel("Fraction who passed")
-    plt.show()
-
-# binary_changepoint(points)
-
-
-#Changepoint detection with window-based search method
-def window_changepoint(points):
-    model = "l2"  
-    algo = rpt.Window(width=40, model=model).fit(points)
     try:
-        bkps = algo.predict(n_bkps=10)
+        algo = rpt.Binseg(model=model).fit(points)
     except Exception as e:
         print(f"Unable to predict for {model=}")
         print("")
         return
+    bkps = algo.predict(n_bkps=5)
     return bkps
 
-#Changepoint detection with dynamic programming search method
-def dynamic_changepoint(points):
+#---------------------------------------------------------------------------
+def window_changepoint(points):
+    # Changepoint detection with window-based search method
+    model = "l2"  
+    try:
+        algo = rpt.Window(width=40, model=model).fit(points)
+    except Exception as e:
+        print(f"Unable to predict for {model=}")
+        print("")
+        return
+    bkps = algo.predict(n_bkps=10)
+    return bkps
 
+#---------------------------------------------------------------------------
+def dynamic_changepoint(points):
+    # Changepoint detection with dynamic programming search method
     model = "l1"  
     try:
         algo = rpt.Dynp(model=model, min_size=3, jump=5).fit(points)
@@ -119,56 +112,76 @@ def dynamic_changepoint(points):
     plt.title('Change Point Detection: Dynamic Programming Search Method')
     plt.show()
 
-# dynamic_changepoint(points)
+#---------------------------------------------------------------------------
+def display(x, y, bkps, median, field="duration", fw=10, fh=5):
+    # Colors are for shaded regions
+    color = cycle(["#f44174", "#4286f4"])
+    alpha = 0.1
+    legend_position = "upper right"
+    
+    fig, ax = plt.subplots(figsize=(fw, fh))
+    # Calc changepoints in 
+    my_bkps = [x[i-1] for i in bkps]
+    # Plot x vs y 
+    plt.plot(x, y, "-o",
+             color="red",
+             label=field+" rate")
+    # Shade the areas demarcated by changepoints
+    for (start, end), col in zip(pairwise(my_bkps), color):
+        plt.axvspan(max(0, start), end, facecolor=col, alpha=alpha)
 
-def xrdisplay(points, bkps, fw, fh):
-    rpt.show.display(points, bkps, figsize=(fw, fh))
-    mplcursors.cursor(hover=True)
-    plt.title('Change Point Detection: Window-Based Search Method')
+#     mplcursors.cursor(hover=True)
+
     plt.xlabel("Duration (m)")
     plt.ylabel("Fraction who passed")
+    # Make room for the title and labels
     plt.subplots_adjust(top=0.90)
     plt.subplots_adjust(bottom=0.15)
     plt.subplots_adjust(left=.1)
     
-def rdisplay(x, y, bkps, fw, fh):
-    color = cycle(["#4286f4", "#f44174"])
-    fig, ax = plt.subplots(figsize=(10, 5))
-    my_bkps = [x[i-1] for i in bkps]
-    plt.plot(x, y, "-o",
-             color="red",
-             label="duration_rate")
-    for (start, end), col in zip(pairwise(my_bkps), color):
-        plt.axvspan(max(0, start - 0.5), end - 0.5, facecolor=col, alpha=0.1)
+    plt.legend(loc=legend_position)
+    
+    # Use axes limits to determine offset of text from vertical lines
+    (xmin, xmax) = ax.get_xlim()
+    (ymin, ymax) = ax.get_ylim()
+    dt0 = (-(xmax - xmin)*.02, ymin+(ymax-ymin)*0.5)
+    dt1 = (dt0[0], ymin+(ymax-ymin)*0.5)
+    
+    # Draw annotated line at median
+    plt.axvline(x=median, linestyle="dotted")
+    plt.text(median+dt0[0], dt0[1], f"{median=:.1f} min", rotation=90)
 
-#     mplcursors.cursor(hover=True)
+    # Draw annotated line at 1st changepoint
+    changept = my_bkps[0]
+    plt.axvline(x=changept, linestyle="dotted")
+    plt.text(changept+dt1[0], dt1[1], f"{changept=:.1f} min", rotation=90)
 
-    plt.title('Change Point Detection: Window-Based Search Method')
-    plt.xlabel("Duration (m)")
-    plt.ylabel("Fraction who passed")
-    plt.subplots_adjust(top=0.90)
-    plt.subplots_adjust(bottom=0.15)
-    plt.subplots_adjust(left=.1)
+#---------------------------------------------------------------------------
 
+field = "duration"
+fieldrate = field + "_rate"
 
-# df.duration.median()
+df, pr, bins = find_passing_rate(case=2, field=field)
+median = df[field].median()
 
-if __name__ == '__main__':
-    df, pr, limits = main()
+points = pd.DataFrame(pr[fieldrate], columns=[fieldrate])
 
-points = pd.DataFrame(pr.duration_rate, columns=["duration_rate"])
-fw,fh = 6,4
+# Use the window-based search method and graph
 my_bkps = window_changepoint(points)
+display(pr.duration, pr[fieldrate], my_bkps, median, field=field)
+plt.title('Change Point Detection: Window-Based Search Method')
+# plt.show()
 
-median = df.duration.median()
-rdisplay(pr.duration, pr.duration_rate, my_bkps, fw, fh)
-plt.axvline(x=median, linestyle="dotted")
-plt.text(median-1.05, .3, f"{median=:.1f} min", rotation=90)
-changept = pr.duration[my_bkps[0]-2]
-plt.axvline(x=changept, linestyle="dotted")
-# plt.axvline(x=pr.duration[my_bkps[0]-2], linestyle="dotted")
-plt.text(changept-1.05, .3, f"{changept=:.1f} min", rotation=90)
+# Use the Binary Segmentation search method dnd graph
+my_bkps1 = binary_changepoint(points)
+display(pr.duration, pr[fieldrate], my_bkps1, median, field=field) 
+plt.title("Change Point Detection: Binary Segmentation search")
+# plt.show()
 
+# Use the Pelt search method and graph
+my_bkps2 = pelt_changepoint(points)
+display(pr.duration, pr[fieldrate], my_bkps2, median, field=field) 
+plt.title("Change Point Detection: Pelt search method")
 plt.show()
 
 
