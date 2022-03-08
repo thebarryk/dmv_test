93b2038ea9ed514b6a04ffeac8b3ce94a5e50aba
Barry Krawchuk <thebarryk@gmail.com>
2022-03-08 07:08:19 -0500

    passing_rate_over_time.py Calculate rates for each day to show change on 10/27/2021. - Removed unneeded lines to find date intervals. All done by resample. - Changed color from red to green - Removed symbol on line to remove clutter and show change more sharply - Generalized names and labels from week to date and period - Rotated the tick labels by 45 deg - Used subplots_adjust to provide room for angles tick labels - Changed the figure size from 8x4 to 6x5 - Changed lower ylim to emphasize change more

--- passing_rate_over_time.py
+++ passing_rate_over_time.py
@@ -1,55 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[56]:
+# In[1]:
 
 
 get_ipython().run_line_magic('matplotlib', 'notebook')
 # Show variation of success vs time spent taking the driver's test
 # as it changes from week to week, or some other period.
 # The passing rate is averaged over bins of time when the test was taken.
+# The rate is calculated for values  > changept
 
 import dmv_test_input as dti
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import mplcursors
+from itertools import cycle
 
 
-# In[57]:
+# In[2]:
 
 
 # Read test logs from alts database
 df = dti.read_dmv_log(case=2)
 df["passed"]  = (df.Result=="P")
 
+# Calculate the rate after the change point
+changept = 14.5
+df = df[ (df.duration >= changept ) & (df.duration <= 40) ].reset_index()
 
-# In[61]:
+
+# In[3]:
 
 
 def plot_passing_rate(pf):
     linestyle = cycle(["-", "--"])
-    color = cycle(["red", "green"])
-    fig, ax = plt.subplots(figsize=(8, 4))
-    plt.plot(pf.week, 
-             pf['rate'],  "o",
+    color = cycle(['green', 'red'])
+    fig, ax = plt.subplots(figsize=(6, 5))
+    plt.plot(pf.period, 
+             pf['rate'],  #"o",
              color=next(color),
              linestyle=next(linestyle), 
              label="Passing rate")
-    
-    ax.set_title(f"Passing Rate by Week")
-    ax.set_xlabel(f"Week")
+    mplcursors.cursor(hover=True)
+    ax.set_title(f"Passing Rate by Date")
+    ax.set_xlabel(f"Date")
     ax.set_ylabel(f"Passing Rate")
+    ax.tick_params(axis='x', rotation=45)
+    fig.subplots_adjust(bottom=.2)
+    ax.set_ylim(bottom=.5)
     ax.legend()
     plt.grid(visible=True)
     plt.show() 
 
 def main():
     
-    mn, mx = df.TestStartDateTime.agg([min, max])
-    weeks = pd.date_range(start=mn, end=mx, freq='W')
-    pf = df.resample('W', on='TestStartDateTime').passed.agg([sum,len]).reset_index()
-    pf.columns=['week', 'p', 'n']
+    pf = df.resample('D', on='TestStartDateTime').passed.agg([sum,len]).reset_index()
+    pf.columns=['period', 'p', 'n']
     pf['rate'] = pf.apply(lambda x: x.p/x.n if x.n != 0 else 0, axis=1)
     
     plot_passing_rate(pf)
@@ -62,10 +69,10 @@ if __name__ == '__main__':
     pf
 
 
-# In[55]:
+# In[4]:
 
 
-pf.head()
+pf.rate[pf.period>='2021-10-27'].mean()
 
 
 # In[ ]:
