35b984525e3d4cd9e231aab1746300892b31b862
Barry Krawchuk <thebarryk@gmail.com>
2022-03-29 12:52:13 -0400

    repair_date_risk.py Use mywhois module after successful testing

--- repair_date_risk.py
+++ repair_date_risk.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 
 # Repair the format of the timestamp and over write the risk database
 
 import pandas as pd
-import xmywhois as mywhois
+import mywhois
 import ipaddress
 import pickle
 import dbm
 
 
-# In[2]:
+# In[7]:
 
 
 class Halt():
@@ -48,7 +48,7 @@ def main():
     return risk
 
 
-# In[3]:
+# In[8]:
 
 
 risk = main()
